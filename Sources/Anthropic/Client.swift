f0a3aeae3713807efca37d803fa2c4b32480a43c
Nathan Borror <nathan@nathanborror.com>
2024-01-17 13:18:26 -0800

    public

--- Sources/Anthropic/Client.swift
+++ Sources/Anthropic/Client.swift
@@ -6,7 +6,7 @@ public final class AnthropicClient {
         public let host: URL
         public let token: String
         
-        init(host: URL = URL(string: "https://api.anthropic.com/v1")!, token: String) {
+        public init(host: URL = URL(string: "https://api.anthropic.com/v1")!, token: String) {
             self.host = host
             self.token = token
         }
