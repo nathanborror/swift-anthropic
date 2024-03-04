6c2bd6f980e0316684b6e3589ae189e294b0e1c1
Nathan Borror <nathan@nathanborror.com>
2024-03-04 07:52:15 -0800

    claude 3 models

--- Sources/Anthropic/Client.swift
+++ Sources/Anthropic/Client.swift
@@ -49,6 +49,8 @@ public final class AnthropicClient {
     public func models() async throws -> ModelListResponse {
         .init(
             models: [
+                "claude-3-opus-20240229",
+                "claude-3-sonnet-20240229",
                 "claude-2.1",
                 "claude-2.0",
                 "claude-instant-1.2",
