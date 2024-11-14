83433823c01677a18b667a71c9e0ef5be6ddc80a
Nathan Borror <nathan@nathanborror.com>
2024-11-13 20:35:19 -0800

    updating models

--- Sources/Anthropic/Defaults.swift
+++ Sources/Anthropic/Defaults.swift
@@ -1,17 +1,24 @@
 import Foundation
 
 public struct Defaults {
-    
+
     public static let apiHost = URL(string: "https://api.anthropic.com/v1")!
     public static let apiVersion = "2023-06-01"
     public static let apiVersionBeta: String? = "max-tokens-3-5-sonnet-2024-07-15"
-    
-    public static let chatModel = "claude-3-5-sonnet-20240620"
-    
+
+    public static let chatModel = "claude-3-5-sonnet-latest"
+
     public static let models: [Model] = [
         .init(
-            id: "claude-3-5-sonnet-20240620",
+            id: "claude-3-5-sonnet-latest",
             name: "Claude 3.5 Sonnet",
+            owner: "anthropic",
+            contextWindow: 200_000,
+            maxOutput: 8192
+        ),
+        .init(
+            id: "claude-3-5-haiku-latest",
+            name: "Claude 3.5 Haiku",
             owner: "anthropic",
             contextWindow: 200_000,
             maxOutput: 8192
